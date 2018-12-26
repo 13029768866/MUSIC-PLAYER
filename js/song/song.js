e15bd52f76b6e3907957b7ae9148d95baf921263
woodensRangerWZJ <13029768866@163.com>
2018-12-26 17:18:31 +0800

    歌词页面渲染

--- js/song/song.js
+++ js/song/song.js
@@ -1,19 +1,23 @@
-
 $(function() {
   $.get("/lyric.json").then(function(response) {
     // let lyric = JSON.parse(response).lyric
     let { lyric } = response;
     let lyricArray = lyric.split("\n");
-    console.log(lyricArray)
+    console.log(lyricArray);
     // 匹配【】内内容
-    let regex = /^\[(.+)\](.+)/;
-    let lyricObj = lyricArray.map(item => {
+    let regex = /^\[(.+)\](.*)$/;
+    lyricArray = lyricArray.map(item => {
       let lyricMatches = item.match(regex);
-      if(lyricMatches){
-        return { time: lyricMatches[1], content: lyricMatches[2]};
+      if (lyricMatches !== null) {
+        return { time: lyricMatches[1], content: lyricMatches[2] };
       }
     });
-    console.log(lyricObj);
-    
+    lyricArray.splice(lyricArray.length-1)
+    let $lyric = $(".lyric")
+    lyricArray.map((item)=>{
+        let $p  = $('<p/>')
+        $p.attr('data-time',item.time).text(item.content)
+        $p.appendTo($lyric)
+    })
   });
 });
