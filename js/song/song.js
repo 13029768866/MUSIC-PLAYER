d55177dd95da0892855262c7d66f4a0a1406718b
woodensRangerWZJ <13029768866@163.com>
2018-12-27 16:17:03 +0800

    歌曲页播放功能

--- js/song/song.js
+++ js/song/song.js
@@ -1,23 +1,21 @@
 $(function() {
   $.get("/lyric.json").then(function(response) {
-    // let lyric = JSON.parse(response).lyric
     let { lyric } = response;
-    let lyricArray = lyric.split("\n");
-    console.log(lyricArray);
-    // 匹配【】内内容
+    let lyricArray = lyric.split('\n')
     let regex = /^\[(.+)\](.*)$/;
-    lyricArray = lyricArray.map(item => {
-      let lyricMatches = item.match(regex);
-      if (lyricMatches !== null) {
-        return { time: lyricMatches[1], content: lyricMatches[2] };
-      }
-    });
+    lyricArray = lyricArray.map((item) => {
+       let matches = item.match(regex)
+       if(matches){
+           return {time:matches[1],content:matches[2]}
+       }
+    })
     lyricArray.splice(lyricArray.length-1)
-    let $lyric = $(".lyric")
-    lyricArray.map((item)=>{
-        let $p  = $('<p/>')
+    // console.log(lyricArray)
+    let $lyric = $('.lyric')
+    lyricArray.map((item) => {
+        let $p = $('<p/>')
         $p.attr('data-time',item.time).text(item.content)
         $p.appendTo($lyric)
     })
-  });
+    })
 });
