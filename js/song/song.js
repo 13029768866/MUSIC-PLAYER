50f5837ff22b64ea7ba48908adea9c83189fabc2
woodensRangerWZJ <13029768866@163.com>
2019-01-03 17:22:05 +0800

    播放暂停功能

--- js/song/song.js
+++ js/song/song.js
@@ -1,7 +1,7 @@
 $(function() {
   $.get("/lyric.json").then(function(response) {
     let { lyric } = response;
-    let lyricArray = lyric.split('\n')
+    let lyricArray = lyric.split('↵')
     let regex = /^\[(.+)\](.*)$/;
     lyricArray = lyricArray.map((item) => {
        let matches = item.match(regex)
@@ -10,12 +10,30 @@ $(function() {
        }
     })
     lyricArray.splice(lyricArray.length-1)
-    // console.log(lyricArray)
+    console.log(lyricArray)
     let $lyric = $('.lyric')
     lyricArray.map((item) => {
         let $p = $('<p/>')
         $p.attr('data-time',item.time).text(item.content)
         $p.appendTo($lyric)
-    })
+    })  
     })
 });
+let audio = document.createElement('audio')
+let status = true;
+audio.src = "http://dl.stream.qqmusic.qq.com/C400003lIzTG3HmKk6.m4a?guid=9408987631&vkey=323808D047AED833F7E8026585808A932A784774B063E94D497029C7058943EF11C8ED813ECE184F4753B499EE5E66A99956B75328E03B26&uin=0&fromtag=38"
+
+$('.song_picture_container').on('click',()=>{
+    if(status){
+        $('#music_play').hide();
+        $('.song_picture_container').addClass('playing')
+        audio.play()
+        status = !status
+    }else{
+        audio.pause()
+        status = !status;
+        $('#music_play').show();
+        $('.song_picture_container').removeClass('playing')
+    }
+    
+})
